41a571d8941992cc949fa2d386ad4ff72c2144c0
Lydie Cherilus <57009304+lydiecherilus@users.noreply.github.com>
2022-06-24 03:37:49 -0400

    Update bill_splitter.py

--- bill_splitter.py
+++ bill_splitter.py
@@ -1,20 +1,32 @@
 ''' The program calculates the total bill each person should pay when dining in group including tip '''
 
 calculation_end = False
-while not calculation_end:
-    print("Welcome to the bill calculator")
-    bill = input("What is the total bill? $")
-    tip = input("What percentage tip would you like to give? 10, 12 or 15? ")
-    total_tip = (float(bill) * float(tip)) / 100
-    people = input("How many people are splitting the bill? ") 
-    person_amount = (float(bill) + total_tip) / float(people)
-    final_person_amount = round(person_amount, 2)
-    final_person_amount = "{:.3f}".format(person_amount)
-    print(f"Each person should pay: ${final_person_amount}")
+is_input_valid = False
+print("Welcome to the bill calculator")
 
-    restart = input('Type "yes" if you want to calculate another bill -- otherwise type "no". \n')
-    if restart == "no":
-        calculation_end = True
-        print("Goodbye")
+while not calculation_end and not is_input_valid: 
+    try:
+        bill = float(input("What was the total bill? $"))
+        tip = float(input("What percentage tip would you like to give? 10, 12 or 15? "))
+        total_tip = (float(bill) * float(tip)) / 100
+        people = float(input("How many people to split the bill? "))
+        is_input_valid = True
+        
+        person_amount = (bill + total_tip) / people
+        final_person_amount = round(person_amount, 2)
+        final_person_amount = "{:.2f}".format(person_amount)
+        print(f"Each person should pay: ${final_person_amount}")
+
+        restart = input('Type "yes" if you want to calculate another bill -- otherwise type "no". \n')
+        if restart == "no":
+            calculation_end = True
+            print("Goodbye")
+        elif restart == "yes":
+            calculation_end = False
+            is_input_valid = False
+
+    except ValueError:
+        print("Please only input digits")
+
         
 # use this link to calculate your bill: https://replit.com/@lydiecherilus/billsplitter?v=1

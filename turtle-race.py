84e262367c296adac45aeaf9335e6657c6ca014d
Lydie Cherilus <57009304+lydiecherilus@users.noreply.github.com>
2022-07-13 10:06:38 -0400

    Update turtle-race.py
    take user bet

--- turtle-race.py
+++ turtle-race.py
@@ -6,5 +6,11 @@ screen = Screen()
 screen.setup(width=500, height=500)
 screen.title("Turtle Racing")
 
+# take user bet
+user_bet = screen.textinput(title="Bet: red, black, green, blue, purple \n", prompt="Which turtle will win the race? Enter a color: ")
+
+if user_bet != "red" or user_bet != "black" or user_bet != "green" or user_bet != "blue" or user_bet != "purple":
+    user_bet = screen.textinput(title="Bet: red, black, green, blue, purple \n", prompt="Which turtle will win the race? Enter a color: ")
+
 
 screen.exitonclick()

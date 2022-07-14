5cb9d8d2f2e068bc76549197bacd5fc12e035e29
Lydie Cherilus <57009304+lydiecherilus@users.noreply.github.com>
2022-07-14 15:57:21 -0400

    create turtles

--- turtle-race.py
+++ turtle-race.py
@@ -15,14 +15,19 @@ game_over.color("deep pink")
 
 # take user bet
 user_bet = screen.textinput(title="Bet: red, black, green, blue, purple \n", prompt="Which turtle will win the race? Enter a color: ")
-
-if user_bet != "red" or user_bet != "black" or user_bet != "green" or user_bet != "blue" or user_bet != "purple":
-    user_bet = screen.textinput(title="Bet: red, black, green, blue, purple \n", prompt="Which turtle will win the race? Enter a color: ")
     
 # turtles color and position
 turtles_colors = ["red", "black", "green", "blue", "purple"]
 y_positions = [-120, -60, 0, 60, 120]
 all_turtles = []
+
+# create turtles
+for turtle_index in range(0, 5):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(turtles_colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
 
 screen.exitonclick()
